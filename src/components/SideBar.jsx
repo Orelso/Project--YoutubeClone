caf2eb5f837f32c005a57579d4e2433802ed0def
Orelso Vitam <orelso.vitam@avanio.com>
2022-12-21 14:56:00 +0200

    another

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,17 +1,16 @@
 import { Stack } from "@mui/material"
 import { categories } from "../utils/constants"
 
-const selectedCategory = 'New'
 
-const SideBar = () =>  (
+const SideBar = ({ selectedCategory, setSelectedCategory}) =>  (
 <Stack
     direction="row"
     sx={{overflowY: "auto", height: { sx: 'auto', md: '95%'}, flexDirection: { md: 'column' }
     }}
     >
         {categories.map((category) => (
-            <button className="category-btn" style={{background: category.name === selectedCategory && '#FC1503', color: 'white'}} key={category.name}> {/*  */}
-                <span style={{ color: category.name === selectedCategory ? 'white' : 'pink', marginRight: '15px'}}>{category.icon}</span>
+            <button className="category-btn" onClick={() => setSelectedCategory (category.name)} style={{background: category.name === selectedCategory && '#FC1503', color: 'white'}} key={category.name}> {/*  */}
+                <span style={{ color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px'}}>{category.icon}</span>
                 <span style={{ opacity: category.name === selectedCategory ? '1' : '0.85'}}>{category.name}</span>
             </button>
         ))}
