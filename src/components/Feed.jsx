a448f14c1ec8e21ee5e0acdb6ee02151461dcd54
Orelso Vitam <orelso.vitam@avanio.com>
2022-12-21 14:14:35 +0200

    another

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { Box, Stack } from '@mui/system'
@@ -7,15 +6,21 @@ import SideBar from './SideBar'
 
 const Feed = () => {
   return (
-    <Stack sx={{ flexDirection: {sx: "column", md: "row"}}}> //* On small devices its a column view on medium and up devices its a row view
-      <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2}}}> //* On small devices view will be auto on medium and higher devices it will be 92vh ---- borderRight creates the line that divides the sidebar and the videos 
+    <Stack sx={{ flexDirection: {sx: "column", md: "row"}}}> {/* On small devices its a column view on medium and up devices its a row view*/}
+      <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2}}}> {/* On small devices view will be auto on medium and higher devices it will be 92vh ---- borderRight creates the line that divides the sidebar and the videos*/}
         <SideBar />
         <Typography 
           className="copyright"
           variant="body2" sx={{ mt: 1.5, color: '#fff'}}
         >
-          Copyright 2022 Orelso
+          &#169;Copyright 2022 Orelso
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white'}}>
+         New <span style={{ color: '#F31503'}}>Videos</span>
+        </Typography>
+        <Videos />
       </Box>
     </Stack>
   )
