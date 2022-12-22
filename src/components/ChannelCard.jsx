7313661f58ae5843548ebb0085b840cc02a29048
Orelso Vitam <orelso.vitam@avanio.com>
2022-12-22 13:21:05 +0200

    another

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Box, CardContent, CardMedia, Typography } from "@mui/material"
+import { CheckCircle } from "@mui/icons-material"
+import { Link } from "react-router-dom"
+import { demoProfilePicture } from "../utils/constants"
 
-const ChannelCard = () => {
-  return (
-    <div>ChannelCard</div>
-  )
-}
+
+const ChannelCard = ({ channelDetail }) => (
+  <Box
+  sx={{ boxShadow: 'none',
+  borderRadius: '20px'
+}}
+  >
+<Link to={`/channel/${channelDetail?.id?.channelId}`}>
+  <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff' }}>
+    <CardMediaimage={}
+  </CardContent>
+
+</Link>
+  </Box>
+)
 
 export default ChannelCard
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,18 +1,29 @@
 import { Link } from "react-router-dom"
-import { Typography, Card, CarContent, CardMedia } from "@mui/material"
+import { Typography, Card, CardContent, CardMedia } from "@mui/material"
 import { CheckCircle } from "@mui/icons-material"
-import { demoThumbnailUrl, demoVideoUrl, DemoVideoTitle, DEmoChannelUrl, demoChannelTitle } from "../utils/constants"
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle,  } from "../utils/constants"
 
 const VideoCard = ({ video: { id:{ videoId }, snippet }  }) => {
   return (
-    <Card>
+    <Card sx={{ width: { md: '320px', xs: '100%'}, boxShadow: 'none', borderRadius: 0}}> {/* on medium devices 320px of width on small devices is 100% width */}
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
       <CardMedia 
         image={snippet?.thumbnails?.high?.url}
         alt={snippet?.title}
         sx={{width: 358, height: 180 }}
-
       />
+      </Link>
+      <CardContent sx={{backgroundColor: '#1e1e1e', height: '80px'}} />
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <Typography variant="subtitle1" fontWeight="bold" color="white" sx={{backgroundColor: '#1e1e1e', display: 'flex', mt: -10}}>
+          {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}  {/* from 0-60 characters */}
+        </Typography>
+      </Link>
+      <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+        <Typography variant="subtitle2" fontWeight="bold" color="gray" sx={{backgroundColor: '#1e1111'}}>
+          {snippet?.channelTitle.slice(0, 60) || demoChannelTitle.slice(0, 60)} <CheckCircle sx={{ fontSize: 12, color: 'green', ml: '5px'}}/> {/* from 0-60 characters */}
+        </Typography>
+      
       </Link>
     </Card>
   )
